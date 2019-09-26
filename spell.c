2a3524f9f2ee57756bdd095c13a77d0bf205b134
joegumke <jdg597@nyu.edu>
2019-09-26 17:28:22 -0500

    updating code for bug fixes in spell.c && adding reports for assignment 1 grading

--- spell.c
+++ spell.c
@@ -59,69 +59,71 @@ bool check_word(const char* word, hashmap_t hashtable[])
 }
 
 // 2 - function to load list of words into the hash map (load_dictionary)
-bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[])
-{   
+bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[5000000]){   
     // Open dict_file from path stored in dictionary.
     FILE *dict_file = fopen(dictionary_file, "r");
 
     //If dict_file is NULL -- return false
-    if(!dict_file){return false;}
+    if(!dict_file){return 0;}
 
-    //Initialize all values in hash table to NULL.
-    for(int w=0; w<=HASH_SIZE;w++){hashtable[w]=NULL;}
+    if(dict_file){
+        //Initialize all values in hash table to NULL.
+        for(int w=0; w<=HASH_SIZE;w++){hashtable[w]=NULL;}
 
-    // Declare variable for hash value
-    int hashvalue;
+        // Declare variable for hash value
+        int hashvalue;
 
-    // Assign the read line a character Array with Length as size
-    char line[LENGTH];
+        // Assign the read line a character Array with Length as size
+        char line[LENGTH];
 
-    //While word in dict_file is not EOF (end of file):
-    while(fgets(line,LENGTH,dict_file)){
-        // strip newline 
-        if(line[strlen(line)-1] =='\n'){line[strlen(line)-1]='\0';}
+        //While word in dict_file is not EOF (end of file):
+        while(fgets(line,LENGTH,dict_file)){
+            // strip newline 
+            if(line[strlen(line)-1] =='\n'){line[strlen(line)-1]='\0';}
         
-        //convert dictionary to lower
-        for(int a=0;a<=strlen(line);a++){line[a]= tolower(line[a]);}
+            //convert dictionary to lower
+            for(int a=0;a<=strlen(line);a++){line[a]= tolower(line[a]);}
         
-        hashvalue = hash_function(line);
-        //  1) get an index by calling hash_function(word), as you did above.
-        if(hashtable[hashvalue] == NULL){
-        //  2) if hashtable[hashvalue] == NULL, then:
-        //      1) create a new node *. Let's call it curr. (malloc, etc)
-                node * curr = malloc(sizeof(node));
-        //      2) set curr->word = word (might need strncopy here).
-                strncpy(curr->word,line,LENGTH);
-        //      3) set curr->next = NULL
-                curr->next = NULL;
-        //      4) set hashtable[hashvalue] = curr
-                hashtable[hashvalue] = curr;
-        }
-        else{
-        //  3) else:
-        //      1) create a new node *. Let's call it curr (malloc, etc)
-                node * curr = malloc(sizeof(node));
-        //      2) set curr->word = word (might need strncopy here).
-                strncpy(curr->word,line,LENGTH);
-        //      3) set curr->next = hashtable[index]
-                curr->next =hashtable[hashvalue];
-        //      4) set hashtable[hashvalue] = curr
-                hashtable[hashvalue] = curr;
+            hashvalue = hash_function(line);
+            //  1) get an index by calling hash_function(word), as you did above.
+            if(hashtable[hashvalue] == NULL){
+            //  2) if hashtable[hashvalue] == NULL, then:
+            //      1) create a new node *. Let's call it curr. (malloc, etc)
+                    node * curr = malloc(sizeof(node));
+            //      2) set curr->word = word (might need strncopy here).
+                    strncpy(curr->word,line,LENGTH);
+            //      3) set curr->next = NULL
+                    curr->next = NULL;
+            //      4) set hashtable[hashvalue] = curr
+                    hashtable[hashvalue] = curr;
             }
+            else{
+            //  3) else:
+            //      1) create a new node *. Let's call it curr (malloc, etc)
+                    node * curr = malloc(sizeof(node));
+            //      2) set curr->word = word (might need strncopy here).
+                    strncpy(curr->word,line,LENGTH);
+            //      3) set curr->next = hashtable[index]
+                    curr->next =hashtable[hashvalue];
+            //      4) set hashtable[hashvalue] = curr
+                    hashtable[hashvalue] = curr;
+                }
+        }
+        fclose(dict_file);
+        return 1;
+        }
+            //Close dict_file.
+        else{return 0;}
     }
-    //Close dict_file.
-    fclose(dict_file);
-    return 1;
-}
 
 // 3 - function to tie the above 2 together (check_words)
 // parameter to check_words will be a file pointer containing lines of words seperated by spaces,punctuation, etc
 // word defined as seperated by spaces && if punctuation in a word, then mispelled
 int check_words(FILE* fp, hashmap_t hashtable[], char* misspelled[1000]){ 
-
     //Check and close if File not correct
     if(!fp){return 1;}
     // set int num_misspelled to 0.
+    if(fp){
     int num_misspelled = 0 ;
 
     //initialize wordBuffer to read file into
@@ -165,7 +167,7 @@ int check_words(FILE* fp, hashmap_t hashtable[], char* misspelled[1000]){
             //printf("%d",wordCheckResult);
             //printf("wordCheckResult: %i\n", wordCheckResult);
             // If word exists, increment num_misspelled
-            if(!(wordCheckResult == 1)){
+            if(!(wordCheckResult == 1 && strlen(wordBuffer) < 500000)){
                 //malloc space in the misspelled element strcopy in?
                 misspelled[num_misspelled] = malloc(strlen(token));
                 strcpy(misspelled[num_misspelled], token);
@@ -173,9 +175,11 @@ int check_words(FILE* fp, hashmap_t hashtable[], char* misspelled[1000]){
                 }
             token = strtok(NULL," ");
             }
+        //printf("MISSPELLED: %i\n",num_misspelled);
+        return num_misspelled;
         }
-    //printf("MISSPELLED: %i\n",num_misspelled);
-    return num_misspelled;
+    }
+        return 0;
 }
 
 //int main(){}
